3e0b7af7d50351a74b9974a6d4bce05d14ec24bf
Aditya Rao <dev.adityarao@gmail.com>
2019-02-24 20:10:51 +0530

    Update dependencies and change body background color

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
-import React, { Component } from "react";
 import { withStyles } from "@material-ui/core/styles";
+import React, { Component } from "react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 
 const styles = {
-  topContainer: {
-    display: "block",
-    width: "100%",
-    height: "100%",
-    minHeight: "100vh",
-    backgroundColor: "#fff",
-    backgroundImage: "linear-gradient(-135deg,#1400c8,#b900b4,#f50000)",
-  }
+	topContainer: {
+		display: "block",
+		width: "100%",
+		height: "100%",
+		minHeight: "100vh",
+		backgroundColor: "#FEB300",
+	}
 };
 class App extends Component {
-  render() {
-    const { classes } = this.props;
-    return (
-      <div className={classes.topContainer}>
-        <Header />
-        <Main />
-      </div>
-    );
-  }
+	render() {
+		const { classes } = this.props;
+		return (
+			<div className={classes.topContainer}>
+				<Header />
+				<Main />
+			</div>
+		);
+	}
 }
 
 export default withStyles(styles)(App);
